ce7fdad7f45230581175401c0311f80e75515669
Wai-Shing Luk <luk036@gmail.com>
2021-11-13 04:41:04 +0000

    reformat using --target fix-format

--- specific.cmake
+++ specific.cmake
@@ -1,27 +1,31 @@
-CPMAddPackage(
-  NAME fmt
-  GIT_TAG 7.1.3
-  GITHUB_REPOSITORY fmtlib/fmt
-  OPTIONS "FMT_INSTALL YES" # create an installable target
+cpmaddpackage(
+  NAME
+  fmt
+  GIT_TAG
+  7.1.3
+  GITHUB_REPOSITORY
+  fmtlib/fmt
+  OPTIONS
+  "FMT_INSTALL YES" # create an installable target
 )
 
-CPMAddPackage(
-  NAME Bingo
-  GIT_TAG 1.0
-  GITHUB_REPOSITORY luk036/bingo-cpp
-  OPTIONS "INSTALL_ONLY ON"
-)
+cpmaddpackage(
+  NAME
+  Bingo
+  GIT_TAG
+  1.0
+  GITHUB_REPOSITORY
+  luk036/bingo-cpp
+  OPTIONS
+  "INSTALL_ONLY ON")
 
-CPMAddPackage(
-  NAME cppcoro
-  GIT_TAG 1.0
-  GITHUB_REPOSITORY luk036/cppcoro
-)
+cpmaddpackage(NAME cppcoro GIT_TAG 1.0 GITHUB_REPOSITORY luk036/cppcoro)
 # print_target_properties(cppcoro)
 if(cppcoro_ADDED)
   message(STATUS "Found cppcoro: ${cppcoro_SOURCE_DIR}")
   add_library(cppcoro::cppcoro INTERFACE IMPORTED GLOBAL)
-  target_include_directories(cppcoro::cppcoro SYSTEM INTERFACE ${cppcoro_SOURCE_DIR}/include)
+  target_include_directories(cppcoro::cppcoro SYSTEM
+                             INTERFACE ${cppcoro_SOURCE_DIR}/include)
 endif(cppcoro_ADDED)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
