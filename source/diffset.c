7238b83c244a12cd50ef71d7ec0d024aae56ecea
Wai-Shing Luk <luk036@gmail.com>
2024-01-09 11:06:14 +0800

    add diffset.c

--- source/diffset.c
+++ source/diffset.c
@@ -14,29 +14,40 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX 20
-#define MAX_N 200
+#define MAX_N 70
+
+#define MIN(a, b) ((a) <= (b)) ? (a) : (b)
 
 //-------------------------------------------------------------
 // GLOBAL VARIABLES
 //-------------------------------------------------------------
-int N, D;
-int a[MAX], b[MAX];
-int THRESHOLD;
-int D_MINUS_1;
-int D_TIMES_D_MINUS_1;
-int N_MINUS_D;
-int N1;
-int SIZE_N;
+typedef struct {
+    int n;
+    int d;
+    int a[MAX];
+    int b[MAX];
+    int threshold;
+    int d_minus_1;
+    int d_times_d_minus_1;
+    int n_minus_d;
+    int n1;
+    int n2;
+    size_t size_n;
+} DiffCover;
 
 //-------------------------------------------------------------
-void PrintD() {
+/**
+ * Prints the generated set to stdout and exits.
+ */
+void PrintD(const DiffCover *diff_cover) {
     // print a
-    for (int i = 1; i <= D; i++) {
-        printf("%d ", a[i]);
+    for (int i = 1; i <= diff_cover->d; i++) {
+        printf("%d ", diff_cover->a[i]);
     }
     printf("\n");
     exit(0);
@@ -45,85 +56,80 @@ void PrintD() {
 /*-----------------------------------------------------------*/
 // FIXED DENSITY
 /*-----------------------------------------------------------*/
-void GenD(int t, int p, int tt, int diffset[]) {
-    int differences[MAX_N];
-    memcpy(differences, diffset, SIZE_N);
+/**
+ * Recursively generates all possible D-sets by building them up one
+ * element at a time.
+ *
+ * t - Current element index being added
+ * p - Previous element index
+ * tt - Triangle number of current index t
+ * diffset[] - Bit array tracking differences between elements
+ */
+void GenD(DiffCover *diff_cover, int t, int p, int tt, int8_t diffset[]) {
+    int8_t differences[MAX_N];
+    memcpy(differences, diffset, diff_cover->size_n);
 
     for (int i = 0; i < t; i++) {
-        int diff = a[t] - a[i];
-        differences[diff] = 1;
-        differences[N - diff] = 1;
+        int diff = diff_cover->a[t] - diff_cover->a[i];
+        differences[MIN(diff, diff_cover->n - diff)] = 1;
     }
-    if (t >= THRESHOLD) {
-        int count = 1;
-        for (int i = 1; i < N; i++) {
+    if (t >= diff_cover->threshold) {
+        int count = 0;
+        for (int i = 1; i <= diff_cover->n2; i++) {
             if (differences[i] != 0) {
                 count++;
             }
         }
-        if (count < N1 + tt) {
+        if (count < diff_cover->n1 + tt) {
             return;
         }
     }
 
     int t1 = t + 1;
-    if (t1 >= D)
-        PrintD();
+    if (t1 >= diff_cover->d)
+        PrintD(diff_cover);
     else {
-        int tail = N_MINUS_D + t1;
-        int max = a[t1 - p] + a[p];
-        int tt1 = t1 * (t1 + 1);
+        int tail = diff_cover->n_minus_d + t1;
+        int max = diff_cover->a[t1 - p] + diff_cover->a[p];
+        int tt1 = t1 * (t1 + 1) / 2;
         if (max <= tail) {
-            a[t1] = max;
-            b[t1] = b[t1 - p];
+            diff_cover->a[t1] = max;
+            diff_cover->b[t1] = diff_cover->b[t1 - p];
 
-            GenD(t1, p, tt1, differences);
-            if (b[t1] == 0) {
-                b[t1] = 1;
-                GenD(t1, t1, tt1, differences);
+            GenD(diff_cover, t1, p, tt1, differences);
+            if (diff_cover->b[t1] == 0) {
+                diff_cover->b[t1] = 1;
+                GenD(diff_cover, t1, t1, tt1, differences);
             }
             tail = max - 1;
         }
-        for (int j = tail; j >= a[t] + 1; j--) {
-            a[t1] = j;
-            b[t1] = 1;
-            GenD(t1, t1, tt1, differences);
+        for (int j = tail; j >= diff_cover->a[t] + 1; j--) {
+            diff_cover->a[t1] = j;
+            diff_cover->b[t1] = 1;
+            GenD(diff_cover, t1, t1, tt1, differences);
         }
     }
 }
 
 /*------------------------------------------------------------*/
 /*------------------------------------------------------------*/
-void Init() {
-    int i, j;
-
-    for (j = 0; j <= D; j++)
-        a[j] = 0;
-    a[D] = N;
-    a[0] = 0; // for convenience
-
-    D_MINUS_1 = D - 1;
-    D_TIMES_D_MINUS_1 = D * (D - 1);
-    N_MINUS_D = N - D;
-    N1 = N - D_TIMES_D_MINUS_1;
-    SIZE_N = N * sizeof(int);
-
-    // for (i = 0; i <= N; i++) {
-    //     matrix[i][i] = 0;
-    //     for (j = 0; j < i; j++) {
-    //         matrix[i][j] = i - j;
-    //         matrix[j][i] = N - i + j;
-    //     }
-    // }
-
-    int differences[MAX_N];
-    memset(differences, 0, SIZE_N);
+/**
+ * Initializes global variables and arrays used for generating necklaces.
+ * Sets up the 'a' and 'b' arrays to track candidate necklaces,
+ * the 'differences' array to track differences between necklace elements,
+ * and key constants used in the generation algorithms.
+ * Calls GenD() recursively to generate all possible necklaces.
+ */
+void Init(DiffCover *diff_cover) {
+    int8_t differences[MAX_N];
+    memset(differences, 0, diff_cover->size_n);
     differences[0] = 1;
 
-    for (j = N - D + 1; j >= (N - 1) / D + 1; j--) {
-        a[1] = j;
-        b[1] = 1;
-        GenD(1, 1, 2, differences);
+    for (int j = diff_cover->n_minus_d + 1;
+         j >= (diff_cover->n - 1) / diff_cover->d + 1; j--) {
+        diff_cover->a[1] = j;
+        diff_cover->b[1] = 1;
+        GenD(diff_cover, 1, 1, 1, differences);
     }
     printf("No solution is found.\n");
 }
@@ -136,15 +142,34 @@ int main(int argc, char **argv) {
         usage();
         return 1;
     }
-    sscanf(argv[1], "%d", &N);
-    sscanf(argv[2], "%d", &D);
-    sscanf(argv[3], "%d", &THRESHOLD);
+    int n, d, threshold;
 
-    if (N > D * (D - 1) + 1) {
+    sscanf(argv[1], "%d", &n);
+    sscanf(argv[2], "%d", &d);
+    sscanf(argv[3], "%d", &threshold);
+
+    if (n > d * (d - 1) + 1) {
         printf("Error: N must be less than D*(D-1)+1\n");
         return 1;
     }
 
-    Init();
+    DiffCover diff_cover;
+    diff_cover.n = n;
+    diff_cover.d = d;
+    diff_cover.threshold = threshold;
+    diff_cover.d_minus_1 = d - 1;
+    diff_cover.d_times_d_minus_1 = d * (d - 1);
+    diff_cover.n_minus_d = n - d;
+    diff_cover.n2 = n / 2;
+    diff_cover.n1 = n / 2 - d * (d - 1) / 2;
+    diff_cover.size_n = (n / 2 + 1) * sizeof(int8_t);
+
+    for (int j = 0; j <= d; j++)
+        diff_cover.a[j] = 0;
+
+    diff_cover.a[d] = n;
+    diff_cover.a[0] = 0; // for convenience
+
+    Init(&diff_cover);
     return 0;
 }
---
+++ source/notes.md
@@ -0,0 +1,24 @@
+ecgen-cpp/source on  master [!?] via C v13.2.0-gcc 
+❯ time ./test2 133 12 1
+48 57 58 65 78 90 93 104 109 127 131 133 
+
+real    1m9.715s
+user    1m9.523s
+sys     0m0.008s
+
+ecgen-cpp/source on  master [!?] via C v13.2.0-gcc took 1m9s 
+❯ time ./test 133 12 1
+48 57 58 65 78 90 93 104 109 127 131 133 
+
+real    1m12.561s
+user    1m12.399s
+sys     0m0.007s
+
+ecgen-rs/experiments on  master [!?] via 🦀 v1.74.0 took 2s 
+❯ time ./diffset 133 12 1
+48 57 58 65 78 90 93 104 109 127 131 133 
+
+real    6m52.780s
+user    6m43.303s
+sys     0m0.301s
+
