2ee2581d65fe510f05bc1dc6105a3a713352b800
Wai-Shing Luk <luk036@gmail.com>
2024-01-12 16:12:31 +0800

    fine tuning cpp

--- experiments/diffset2.cpp
+++ experiments/diffset2.cpp
@@ -30,23 +30,21 @@ struct DiffCover {
     int d;
     int a[MAX];
     int b[MAX];
+    int differences[MAX_N];
     int threshold;
-    int d_minus_1;
-    int d_times_d_minus_1;
     int n_minus_d;
     int n1;
     int n2;
-    const int *begin_a;
-    size_t size_n;
 
     DiffCover(int n, int d, int threshold)
-        : n{n}, d{d}, threshold{threshold}, d_minus_1{d - 1},
-          d_times_d_minus_1{d * (d - 1)}, n_minus_d{n - d},
-          n1{n / 2 - d * (d - 1) / 2}, n2{n / 2}, begin_a{&a[0]},
-          size_n{(n / 2 + 1) * sizeof(int8_t)} {
+        : n{n}, d{d}, threshold{threshold}, n_minus_d{n - d},
+          n1{n / 2 - d * (d - 1) / 2}, n2{n / 2} {
+
+        for (auto j = 0; j <= this->n2; j++)
+            differences[j] = 0;
+
         for (auto j = 0; j <= d; j++)
             a[j] = 0;
-
         a[d] = n;
         a[0] = 0; // for convenience
     }
@@ -74,52 +72,60 @@ struct DiffCover {
      * t - Current element index being added
      * p - Previous element index
      * tt - Triangle number of current index t
-     * diffset[] - Bit array tracking differences between elements
      */
-    void GenD(int t, int p, int tt, int8_t diffset[]) {
-        int8_t differences[MAX_N];
-        memcpy(differences, diffset, this->size_n);
-
-        const auto at = this->a[t];
-        for (auto p = this->begin_a; p != this->begin_a + t; ++p) {
-            const auto diff = at - *p;
-            const auto n_diff = this->n - diff;
-            differences[diff <= n_diff ? diff : n_diff] = 1;
+    void GenD(int t, int p, int tt) {
+        int diff_t[MAX];
+        auto at = this->a[t];
+        for (auto i = 0; i < t; i++) {
+            auto diff = at - this->a[i];
+            auto n_diff = this->n - diff;
+            auto min_d = diff <= n_diff ? diff : n_diff;
+            diff_t[i] = min_d;
+            this->differences[min_d] += 1;
         }
         if (t >= this->threshold) {
-            int8_t count = 0;
-            const int8_t *begin = &differences[1];
+            auto count = 0;
+            int *begin = &this->differences[1];
             for (auto p = begin; p != begin + this->n2; ++p) {
-                count += *p;
+                if (*p != 0) {
+                    count++;
+                }
             }
-            if (int(count) < this->n1 + tt) {
+            if (count < this->n1 + tt) {
+                int *begin = &diff_t[0];
+                for (auto p = begin; p != begin + t; ++p) {
+                    this->differences[*p] -= 1;
+                }
                 return;
             }
         }
 
-        const auto t1 = t + 1;
+        auto t1 = t + 1;
         if (t1 >= this->d)
             this->PrintD();
         else {
             auto tail = this->n_minus_d + t1;
-            const auto max = this->a[t1 - p] + this->a[p];
-            const auto tt1 = t1 * (t1 + 1) / 2;
+            auto max = this->a[t1 - p] + this->a[p];
+            auto tt1 = t1 * (t1 + 1) / 2;
             if (max <= tail) {
                 this->a[t1] = max;
                 this->b[t1] = this->b[t1 - p];
-
-                this->GenD(t1, p, tt1, differences);
+                this->GenD(t1, p, tt1);
                 if (this->b[t1] == 0) {
                     this->b[t1] = 1;
-                    this->GenD(t1, t1, tt1, differences);
+                    this->GenD(t1, t1, tt1);
                 }
                 tail = max - 1;
             }
-            for (auto j = tail; j >= at + 1; j--) {
+            for (auto j = tail; j >= this->a[t] + 1; j--) {
                 this->a[t1] = j;
                 this->b[t1] = 1;
-                this->GenD(t1, t1, tt1, differences);
+                this->GenD(t1, t1, tt1);
             }
+        }
+        int *begin = &diff_t[0];
+        for (int *p = begin; p != begin + t; p++) {
+            this->differences[*p] -= 1;
         }
     }
 
@@ -133,15 +139,13 @@ struct DiffCover {
      * Calls GenD() recursively to generate all possible necklaces.
      */
     void run() {
-        int8_t differences[MAX_N];
-        memset(differences, 0, this->size_n);
-        differences[0] = 1;
+        this->differences[0] = 1;
 
         for (auto j = this->n_minus_d + 1; j >= (this->n - 1) / this->d + 1;
              j--) {
             this->a[1] = j;
             this->b[1] = 1;
-            this->GenD(1, 1, 1, differences);
+            this->GenD(1, 1, 1);
         }
         printf("No solution is found.\n");
     }
