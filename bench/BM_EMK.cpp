9f2d857e96210582148032699be40a69adcd581e
Wai-Shing Luk <luk036@gmail.com>
2024-03-17 08:59:27 +0000

    add documenta

--- bench/BM_EMK.cpp
+++ bench/BM_EMK.cpp
@@ -1,13 +1,15 @@
-// #include <algorithm> // for fill_n
 #include <ecgen/combin.hpp>
 #include <ecgen/combin_old.hpp>
 
 #include "benchmark/benchmark.h" // for BENCHMARK, State, BENCHMARK_...
 
 /**
- * @brief
- *
- * @param[in,out] state
+ * The function `emk_new` generates combinations of size K from a set of size N and measures its
+ * performance using Google Benchmark.
+ * 
+ * @param state The `state` parameter in the `emk_new` function is of type `benchmark::State &`. It is
+ * used to control the benchmark execution and provide information about the benchmark state, such as
+ * the number of iterations to run, the elapsed time, and other benchmark-related data.
  */
 static void emk_new(benchmark::State &state) {
     constexpr int N = 16;
@@ -27,9 +29,12 @@ BENCHMARK(emk_new);
 //~~~~~~~~~~~~~~~~
 
 /**
- * @brief Define another benchmark
- *
- * @param[in,out] state
+ * The function `emk_old` generates combinations of size K from a set of size N and measures its
+ * performance using Google Benchmark.
+ * 
+ * @param state The `state` parameter in the `emk_new` function is of type `benchmark::State &`. It is
+ * used to control the benchmark execution and provide information about the benchmark state, such as
+ * the number of iterations to run, the elapsed time, and other benchmark-related data.
  */
 static void emk_old(benchmark::State &state) {
     constexpr int N = 16;
