4166c2e28911173512acca122c30920ad332fc14
Wai-Shing Luk <luk036@gmail.com>
2026-06-15 15:39:34 +0800

    remove /wd4996

--- xmake.lua
+++ xmake.lua
@@ -26,7 +26,7 @@ if is_plat("linux") then
 		add_sysincludedirs(termux_prefix .. "/include", { public = true })
 	end
 elseif is_plat("windows") then
-	add_cxflags("/utf-8 /W4 /WX /wd5285 /wd4127 /wd4996", { force = true })
+	add_cxflags("/utf-8 /W4 /WX /wd5285 /wd4127", { force = true })
 end
 
 target("Ecgen")
